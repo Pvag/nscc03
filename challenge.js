0194adb5466c5e43992cb81267a86379ad9a3631
Pvag <paolondon@gmail.com>
2022-11-15 19:25:10 +0100

    feat: testing paraphernalia for challenge #3

---
+++ challenge.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+const letterCombinations = function(digits) {
+  // Write your code here
+}
+
+module.exports = letterCombinations
---
+++ challenge.test.js
@@ -0,0 +1,14 @@
+const letterCombinations = require('./challenge')
+
+describe('Netstrada Coding Challenge #3 - Testing your solution', () => {
+	it.each([
+		['23', ["ad","ae","af","bd","be","bf","cd","ce","cf"]],
+		['', []],
+		['2', ["a", "b", "c"]],
+		['234', ["adg","adh","adi","aeg","aeh","aei","afg","afh","afi","bdg","bdh","bdi","beg","beh","bei","bfg","bfh","bfi","cdg","cdh","cdi","ceg","ceh","cei","cfg","cfh","cfi"]],
+		['5678', ["jmpt","jmpu","jmpv","jmqt","jmqu","jmqv","jmrt","jmru","jmrv","jmst","jmsu","jmsv","jnpt","jnpu","jnpv","jnqt","jnqu","jnqv","jnrt","jnru","jnrv","jnst","jnsu","jnsv","jopt","jopu","jopv","joqt","joqu","joqv","jort","joru","jorv","jost","josu","josv","kmpt","kmpu","kmpv","kmqt","kmqu","kmqv","kmrt","kmru","kmrv","kmst","kmsu","kmsv","knpt","knpu","knpv","knqt","knqu","knqv","knrt","knru","knrv","knst","knsu","knsv","kopt","kopu","kopv","koqt","koqu","koqv","kort","koru","korv","kost","kosu","kosv","lmpt","lmpu","lmpv","lmqt","lmqu","lmqv","lmrt","lmru","lmrv","lmst","lmsu","lmsv","lnpt","lnpu","lnpv","lnqt","lnqu","lnqv","lnrt","lnru","lnrv","lnst","lnsu","lnsv","lopt","lopu","lopv","loqt","loqu","loqv","lort","loru","lorv","lost","losu","losv"]]])('should work with digits "%d"', (digits, expected) => {
+		const output = letterCombinations(digits)
+
+		expect(output).toEqual(expected)
+	})
+})
